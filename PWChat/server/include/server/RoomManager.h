5c17461da411d3989a671cd098c6da66d4747a4f
Sara Fojt <sara.fojt167@gmail.com>
2026-03-29 23:58:41 +0200

    ➕ added: initial declaration of RoomManager

---
+++ PWChat/server/include/server/RoomManager.h
@@ -0,0 +1,20 @@
+#ifndef ROOMMANAGER_H
+#define ROOMMANAGER_H
+
+#include <string>
+#include <memory>
+#include <map>
+#include "Room.h"
+
+class RoomManager {
+public:
+    std::map<std::string, std::shared_ptr<Room>> allRooms();
+    std::shared_ptr<Room> getRoom(std::string name);
+    void createRoom(std::string name, bool isPrivate, std::shared_ptr<User> owner, bool ownerIsAdmin);
+    void removeRoom(std::string name);
+
+private:
+    std::map<std::string, std::shared_ptr<Room>> m_allRooms;
+};
+
+#endif // ROOMMANAGER_H
