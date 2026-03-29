a1e68f82c5885bd18d2790b6839168a84aad4679
Sara Fojt <sara.fojt167@gmail.com>
2026-03-29 18:15:51 +0200

    ➕ added: all methods for Rooms

--- PWChat/server/include/server/Room.h
+++ PWChat/server/include/server/Room.h
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 #include <memory>
+#include "common/Packet.h"
 
 class Session;
 class User;
@@ -12,11 +13,14 @@ class Room {
 public:
     virtual ~Room() = default;
 
+    uint32_t id();
     std::string name();
     void addClient(std::shared_ptr<Session> clientToAdd);
     void removeClient(std::shared_ptr<Session> clientToRemove);
     virtual bool canJoin(const std::string& token) = 0;
+    void broadcast(const Packet& p);
 protected:
+    uint32_t m_id;
     std::string m_name;
     std::vector<std::shared_ptr<Session>> m_clients;
     std::vector<std::shared_ptr<User>> m_admins;
---
+++ PWChat/server/src/Room.cpp
@@ -0,0 +1,41 @@
+#include "server/Room.h"
+#include "server/Session.h"
+#include <algorithm>
+#include <vector>
+#include <iostream>
+
+uint32_t Room::id() {
+    return m_id;
+}
+
+std::string Room::name() {
+    return m_name;
+}
+
+void Room::addClient(std::shared_ptr<Session> clientToAdd) {
+    auto it = std::find(m_clients.begin(), m_clients.end(), clientToAdd);
+
+    if (it != m_clients.end()) {
+        std::cerr << "Error: User " << clientToAdd->userId() << " is already in room " << m_name << std::endl;
+    } else {
+        m_clients.push_back(clientToAdd);
+    }
+}
+
+void Room::removeClient(std::shared_ptr<Session> clientToRemove) {
+    auto it = std::find(m_clients.begin(), m_clients.end(), clientToRemove);
+
+    if (it!= m_clients.end()) {
+        m_clients.erase(it, m_clients.end());
+    } else {
+        std::cerr << "Error: User " << clientToRemove->userId() << " wasn't part of the room " << m_name << std::endl;
+    }
+}
+
+void Room::broadcast(const Packet& p) {
+    for (auto& client : m_clients) {
+        if (client->userId() != p.header().senderId) {
+            client->deliver(p);
+        }
+    }
+}
